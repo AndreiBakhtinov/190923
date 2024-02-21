87f6bcd562403cd5ea88b59db10fba1898198247
Natalia Smolnikova <ntnatali@gmail.com>
2024-02-21 20:48:46 +0100

    update

--- smolniknata/orders.sql
+++ smolniknata/orders.sql
@@ -1,7 +1,4 @@
-SELECT * FROM Orders
-Where ShipperID = 1
-git add
+
 SELECT * FROM Orders
 join Shippers on Shippers.ShipperID = Orders.ShipperID
 Where Shippers.ShipperName = "Speedy Express"
-Group By Orders.OrderID
