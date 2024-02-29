45881679406ba3877125d40f0325fde0760a745d
Olga_Pashora <olga.pashora@gmail.com>
2024-02-29 11:40:26 +0100

    update 2024.02.29 task_2 pashora request

--- olga_pashora/task_2_request.sql
+++ olga_pashora/task_2_request.sql
@@ -1,6 +1,12 @@
 SELECT
  Customers.CustomerName,
- COUNT(*) as count_orders
+ count(*) as count_orders
 FROM Orders
-join Customers on Customers.CustomerID = Orders.CustomerID
-group by Orders.CustomerID
+JOIN Customers on Customers.CustomerID = Orders.CustomerID
+GROUP BY Orders.CustomerID
+ORDER BY count_orders DESC
+LIMIT 3
+
+--Вывести ТОП-3 клиентов по ко-ву заказов
+--    имя_клиента
+--    ко-во_заказов
