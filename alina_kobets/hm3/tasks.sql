5d9bf21d7159a8e3566d37f3fd78cc56368c5c49
AlinaKobetss <alinakai88@gmail.com>
2024-03-05 21:02:02 +0100

    hw3

---
+++ alina_kobets/hm3/tasks.sql
@@ -0,0 +1,15 @@
+-- 1. Вывести один товар, который находится на третьем месте среди самых дорогих
+
+SELECT ProductName
+FROM Products
+ORDER BY Price DESC
+LIMIT 1
+OFFSET 2
+
+-- 2. Вывести ТОП-3 клиентов по ко-ву заказов
+
+SELECT CustomerID, COUNT(OrderID) as count_orders
+FROM Orders
+GROUP BY CustomerID
+ORDER BY count_orders DESC
+LIMIT 3
