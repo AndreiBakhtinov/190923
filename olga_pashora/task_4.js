3d842c04a81ce929de70c43216be19af744bce50
Olga_Pashora <olga.pashora@gmail.com>
2024-03-11 14:33:24 +0100

    add task_4.js MongoDB

---
+++ olga_pashora/task_4.js
@@ -0,0 +1,7 @@
+//Задача. Вывести юзеров не из USA и не из UK, имена которых не Ivan Ivanov//
+db.users.find(
+    {
+        country: { $nin: ['USA', 'UK'] },
+        fullname: { $ne: 'Ivan Ivanov' }
+    }
+)
